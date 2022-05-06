98de606f75f5dbda3b5838ed2e2ef1a5548c78e3
xiuwenli <xl3335@nyu.edu>
2022-05-06 16:03:27 -0400

    [FIX] redirect to signin after signup succeed

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -53,7 +53,7 @@ export default function SignUp() {
       .then(res => {
         const data = res.data;
         alert('Signup Success!');
-        window.location = '/';
+        window.location = '/signin';
       }).catch( error => {
         alert('Network error');
       });
